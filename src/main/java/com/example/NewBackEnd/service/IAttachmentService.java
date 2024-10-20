6e3025b29f6fe66dc29eaf1d79eab8e4236b7903
atrungbg <Trungnvse160416@fpt.edu.vn>
2024-10-20 15:22:10 +0700

    fix api

--- src/main/java/com/example/NewBackEnd/service/IAttachmentService.java
+++ src/main/java/com/example/NewBackEnd/service/IAttachmentService.java
@@ -1,16 +1,16 @@
-package com.example.NewBackEnd.service;
-
-import com.example.NewBackEnd.dto.request.attachment.AttachmentRequest;
-import com.example.NewBackEnd.dto.request.note.CreateNoteRequest;
-import com.example.NewBackEnd.dto.request.note.UpdateNoteRequest;
-import com.example.NewBackEnd.dto.response.attachment.AttachmentResponse;
-import com.example.NewBackEnd.dto.response.note.NoteResponse;
-import com.example.NewBackEnd.exception.BaseException;
-
-import java.util.UUID;
-
-public interface IAttachmentService extends IGenericService<AttachmentResponse> {
-    AttachmentResponse createAttachment(AttachmentRequest attachmentRequest) throws BaseException;
-    AttachmentResponse updateAttachment(UUID id, AttachmentRequest attachmentRequest) throws BaseException;
-    boolean deleteAttachment(UUID id) throws BaseException;
-}
+//package com.example.NewBackEnd.service;
+//
+//import com.example.NewBackEnd.dto.request.attachment.AttachmentRequest;
+//import com.example.NewBackEnd.dto.request.note.CreateNoteRequest;
+//import com.example.NewBackEnd.dto.request.note.UpdateNoteRequest;
+//import com.example.NewBackEnd.dto.response.attachment.AttachmentResponse;
+//import com.example.NewBackEnd.dto.response.note.NoteResponse;
+//import com.example.NewBackEnd.exception.BaseException;
+//
+//import java.util.UUID;
+//
+//public interface IAttachmentService extends IGenericService<AttachmentResponse> {
+//    AttachmentResponse createAttachment(AttachmentRequest attachmentRequest) throws BaseException;
+//    AttachmentResponse updateAttachment(UUID id, AttachmentRequest attachmentRequest) throws BaseException;
+//    boolean deleteAttachment(UUID id) throws BaseException;
+//}
